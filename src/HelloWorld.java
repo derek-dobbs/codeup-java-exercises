29e3702bf9527a63b3ab2e6635b78c8787b380c4
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2021-12-14 15:22:32 -0600

    Updated exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,15 +1,28 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello ");
-        System.out.print("World!");
+//        System.out.print("Hello ");
+//        System.out.print("World!");
+//
+//        System.out.println();
 
-        System.out.println();
+        //TODONE: Create an int variable named myFavoriteNumber and assign your favorite number to it, then print it out to the console.
+        int myFavoriteNumber = 83;
+        System.out.println(myFavoriteNumber);
 
-        //TODO: Create an int variable named myFavoriteNumber and assign your favorite number to it, then print it out to the console.
-        //TODO: Create a String variable named myString and assign a string value to it, then print the variable out to the console.
-        //TODO: Change your code to assign a character value to myString. What do you notice?
-        //TODO: Change your code to assign the value 3.14159 to myString. What happens?
+        //TODONE: Create a String variable named myString and assign a string value to it, then print the variable out to the console.
+        String myString = "Derek";
+        System.out.println(myString);
+
+        //TODONE: Change your code to assign a character value to myString. What do you notice?
+        //I noticed that there was an error because the required data type is String
+//        myString = 'd';
+
+        //TODONE: Change your code to assign the value 3.14159 to myString. What happens?
+        //I noticed that there was an error because the required data type is String
+//        myString = 3.14159;
+
         //TODO: Declare an long variable named myNumber, but do not assign anything to it. Next try to print out myNumber to the console. What happens?
+
         //TODO: Change your code to assign the value 3.14 to myNumber. What do you notice?
         //TODO: Change your code to assign the value 123L (Note the 'L' at the end) to myNumber.
         //TODO: Change your code to assign the value 123 to myNumber.
