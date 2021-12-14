c8c3879a853ec2a376c45e0c6cd922fd4ade1c9b
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2021-12-14 15:49:11 -0600

    Completed exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,11 +7,11 @@ public class HelloWorld {
 
         //TODONE: Create an int variable named myFavoriteNumber and assign your favorite number to it, then print it out to the console.
         int myFavoriteNumber = 83;
-        System.out.println(myFavoriteNumber);
+//        System.out.println(myFavoriteNumber);
 
         //TODONE: Create a String variable named myString and assign a string value to it, then print the variable out to the console.
         String myString = "Derek";
-        System.out.println(myString);
+//        System.out.println(myString);
 
         //TODONE: Change your code to assign a character value to myString. What do you notice?
         //I noticed that there was an error because the required data type is String
@@ -21,14 +21,28 @@ public class HelloWorld {
         //I noticed that there was an error because the required data type is String
 //        myString = 3.14159;
 
-        //TODO: Declare an long variable named myNumber, but do not assign anything to it. Next try to print out myNumber to the console. What happens?
+        //TODONE: Declare an long variable named myNumber, but do not assign anything to it. Next try to print out myNumber to the console. What happens?
+        // received error message: java: variable myNumber might not have been initialized
+        long myNumber;
+//        System.out.println(myNumber);
+        //TODONE: Change your code to assign the value 3.14 to myNumber. What do you notice?
+        //the data provided was a double and needs to be a long
+//        myNumber = 3.14;
+//        System.out.println(myNumber);
 
-        //TODO: Change your code to assign the value 3.14 to myNumber. What do you notice?
-        //TODO: Change your code to assign the value 123L (Note the 'L' at the end) to myNumber.
-        //TODO: Change your code to assign the value 123 to myNumber.
-        //TODO: Why does assigning the value 3.14 to a variable declared as a long not compile, but assigning an integer value does?
-        //TODO: Change your code to declare myNumber as a float. Assign the value 3.14 to it. What happens? What are two ways we could fix this?
-        //TODO: Copy and paste the following code blocks one at a time and execute them
+        //TODONE: Change your code to assign the value 123L (Note the 'L' at the end) to myNumber.
+//        myNumber = 123L;
+//        System.out.println(myNumber);
+
+        //TODONE: Change your code to assign the value 123 to myNumber.
+//        myNumber = 123;
+//        System.out.println(myNumber);
+
+        //TODONE: Why does assigning the value 3.14 to a variable declared as a long not compile, but assigning an integer value does?
+        //I am not sure because it compiled as a long
+
+        //TODONE: Change your code to declare myNumber as a float. Assign the value 3.14 to it. What happens? What are two ways we could fix this?
+        //TODONE: Copy and paste the following code blocks one at a time and execute them
         //int x = 5;
         //System.out.println(x++);
         //System.out.println(x);
@@ -39,32 +53,51 @@ public class HelloWorld {
 
         //What is the difference between the above code blocks? Explain why the code outputs what it does.
 
-        //TODO: Try to create a variable named class. What happens?
-        //TODO: Object is the most generic type in Java. You can assign any value to a variable of type Object. What do you think will happen when the following code is run?
-        //String theNumberThree = "three";
-        //Object o = theNumberThree;
-        //int three = (int) o;
+        //TODONE: Try to create a variable named class. What happens?
+        //It doesn't work because class is a reserved key word
+//        class color = "blue";
+
+        //TODONE: Object is the most generic type in Java. You can assign any value to a variable of type Object. What do you think will happen when the following code is run?
+        //I don't think it will compile because int three is casting an int to a String
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//
+//        System.out.println(three);
 
         //Copy and paste the code above and then run it. Does the result match with your expectation?
         //
         //How is the above example different from the code block below?
-        //int three = (int) "three";
+//        int three = (int) "three";
+//
+//        System.out.println(three);
         //What are the two different types of errors we are observing?
 
-        //TODO: Rewrite the following expressions using the relevant shorthand assignment operators:
-        //int x = 4;
-        //x = x + 5;
+        //TODONE: Rewrite the following expressions using the relevant shorthand assignment operators:
+//        int x = 4;
+//        x = x + 5;
 
-        //int x = 3;
-        //int y = 4;
-        //y = y * x;
+//        x += 5;
+//        System.out.println(x);
 
-        //int x = 10;
-        //int y = 2;
-        //x = x / y;
-        //y = y - x;
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
 
-        //TODO: What happens if you assign a value to a numerical variable that is larger (or smaller) than the type can hold? What happens if you increment a numeric variable past the type's capacity?
+        // y *= x;
+//
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+
+        //x /= y;
+
+//        y = y - x;
+
+//        y -= x;
+
+        //TODONE: What happens if you assign a value to a numerical variable that is larger (or smaller) than the type can hold? What happens if you increment a numeric variable past the type's capacity?
         //
         //Hint: Integer.MAX_VALUE is a class constant (we'll learn more about these later) that holds the maximum value for the int type.
     }//end main
