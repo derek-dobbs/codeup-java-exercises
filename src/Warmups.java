4eb73c51c24c16ce736c6e2a8673b81bdecce37e
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2021-12-15 09:05:52 -0600

    Completed morning warmup

---
+++ src/Warmups.java
@@ -0,0 +1,6 @@
+public class Warmups {
+    public static void main(String[] args) {
+        //December 15, 2021
+        System.out.println("Java is statically typed, while Javascript is dynamically typed.");
+    }
+}
