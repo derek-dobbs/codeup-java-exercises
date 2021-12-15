9900c71932280f4cb12383099e42939021190c0d
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2021-12-15 11:41:04 -0600

    Completed problem

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -14,7 +14,7 @@ public class ConsoleExercises {
          */
         double pi = 3.14159;
 
-        System.out.format("The value of pi is approximately %.2f%n", pi);
+//        System.out.format("The value of pi is approximately %.2f%n", pi);
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //        Prompt a user to enter a integer and store that value in an int variable using the nextInt method.
 //        What happens if you input something that is not an integer?
@@ -34,18 +34,23 @@ public class ConsoleExercises {
         What happens if you enter less than 3 words?
         What happens if you enter more than 3 words?
          */
-        System.out.println("\t\t\tEnter three words");
+//        System.out.println("\t\t\tEnter three words");
+//
+//        System.out.print("Enter the first word: ");
+//        String firstWord = scanner.nextLine();
+//
+//        System.out.printf("%nEnter the second word: ");
+//        String secondWord = scanner.nextLine();
+//
+//        System.out.printf("%nEnter the third word: ");
+//        String thirdWord = scanner.nextLine();
+//
+//        System.out.printf("You entered: %n%s%n%s%n%s", firstWord, secondWord, thirdWord);
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        System.out.print("Enter a sentence: ");
+        String userSentence = scanner.nextLine();
 
-        System.out.print("Enter the first word: ");
-        String firstWord = scanner.nextLine();
-
-        System.out.printf("%nEnter the second word: ");
-        String secondWord = scanner.nextLine();
-
-        System.out.printf("%nEnter the third word: ");
-        String thirdWord = scanner.nextLine();
-
-        System.out.printf("You entered: %n%s%n%s%n%s", firstWord, secondWord, thirdWord);
+        System.out.printf("%nYou entered: %s.", userSentence);
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     }//end main
