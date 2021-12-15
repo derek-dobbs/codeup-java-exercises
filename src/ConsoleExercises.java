99c49e972f58e91fdb323b295c0dd0c835141a89
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2021-12-15 11:29:37 -0600

    Completed problem

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -14,7 +14,6 @@ public class ConsoleExercises {
          */
         double pi = 3.14159;
 
-//        System.out.printf("The value of pi is approximately %s.", pi);
         System.out.format("The value of pi is approximately %.2f%n", pi);
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //        Prompt a user to enter a integer and store that value in an int variable using the nextInt method.
@@ -23,20 +22,31 @@ public class ConsoleExercises {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter an integer: ");
+//        System.out.print("Enter an integer: ");
 //        String userInput = scanner.next();
-        int userInput = scanner.nextInt();
+//        int userInput = scanner.nextInt();
 
-        System.out.println("You entered: --> \"" + userInput + "\" <--");
+//        System.out.println("You entered: --> \"" + userInput + "\" <--");
+//        System.out.printf("You entered: %d%n", userInput);
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         /*
         Prompt a user to enter 3 words, and store each of them in a separate variable. Then, display them back in the console, each on a newline.
         What happens if you enter less than 3 words?
         What happens if you enter more than 3 words?
          */
+        System.out.println("\t\t\tEnter three words");
 
+        System.out.print("Enter the first word: ");
+        String firstWord = scanner.nextLine();
+
+        System.out.printf("%nEnter the second word: ");
+        String secondWord = scanner.nextLine();
+
+        System.out.printf("%nEnter the third word: ");
+        String thirdWord = scanner.nextLine();
+
+        System.out.printf("You entered: %s, %s, and %s", firstWord, secondWord, thirdWord);
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 
     }//end main
 }//end class
