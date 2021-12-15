4172d7b0aa2be8a68e6f0cc667ed1ab18a2af10d
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2021-12-15 09:55:55 -0600

    Updated exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,11 +7,11 @@ public class HelloWorld {
 
         //TODONE: Create an int variable named myFavoriteNumber and assign your favorite number to it, then print it out to the console.
         int myFavoriteNumber = 83;
-//        System.out.println(myFavoriteNumber);
+        System.out.println(myFavoriteNumber);
 
         //TODONE: Create a String variable named myString and assign a string value to it, then print the variable out to the console.
         String myString = "Derek";
-//        System.out.println(myString);
+        System.out.println(myString);
 
         //TODONE: Change your code to assign a character value to myString. What do you notice?
         //I noticed that there was an error because the required data type is String
@@ -23,7 +23,7 @@ public class HelloWorld {
 
         //TODONE: Declare an long variable named myNumber, but do not assign anything to it. Next try to print out myNumber to the console. What happens?
         // received error message: java: variable myNumber might not have been initialized
-        long myNumber;
+//        long myNumber;
 //        System.out.println(myNumber);
         //TODONE: Change your code to assign the value 3.14 to myNumber. What do you notice?
         //the data provided was a double and needs to be a long
@@ -44,12 +44,14 @@ public class HelloWorld {
         //TODONE: Change your code to declare myNumber as a float. Assign the value 3.14 to it. What happens? What are two ways we could fix this?
         //TODONE: Copy and paste the following code blocks one at a time and execute them
         //int x = 5;
-        //System.out.println(x++);
-        //System.out.println(x);
+        //System.out.println(x++); //returns 5
+        //System.out.println(x); // returns 6
+        float myNumber = 3.14f;
+//        System.out.println(myNumber);
 
         //int x = 5;
-        //System.out.println(++x);
-        //System.out.println(x);
+        //System.out.println(++x); //returns 6
+        //System.out.println(x); //returns 6
 
         //What is the difference between the above code blocks? Explain why the code outputs what it does.
 
@@ -98,7 +100,9 @@ public class HelloWorld {
 //        y -= x;
 
         //TODONE: What happens if you assign a value to a numerical variable that is larger (or smaller) than the type can hold? What happens if you increment a numeric variable past the type's capacity?
-        //
+        //There will be a compile error
+
+
         //Hint: Integer.MAX_VALUE is a class constant (we'll learn more about these later) that holds the maximum value for the int type.
     }//end main
 }
