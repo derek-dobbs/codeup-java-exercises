3ba1b2093019edc638336f1aa7cf57b388948827
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2021-12-16 11:32:35 -0600

    Updated exercise

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,22 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        int i = 5;
+        while (i <= 15) {
+//            System.out.print(i + " ");
+            i++;
+        }
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        System.out.println();
+        int j = 100;
+        do {
+            if(j % 5 == 0) {
+//                System.out.println(j);
+            }
+            j--;
+        }while(j >= -10);
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        do {
+
+        }while ();
+    }//end main
+}// end class
