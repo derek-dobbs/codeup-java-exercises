2807dbc819f6833d6737c3d88b0ec8faba175c69
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2021-12-16 09:06:48 -0600

    Completed warmup

--- src/Warmups.java
+++ src/Warmups.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
 public class Warmups {
     public static void main(String[] args) {
         //December 15, 2021
         System.out.println("Java is statically typed, while Javascript is dynamically typed.");
-    }
-}
+
+        //December 16, 2021
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What did you eat for breakfast today?");
+        String userInput = scanner.nextLine();
+
+        System.out.printf("You ate %s for breakfast today", userInput);
+    }// end main
+}//end class
