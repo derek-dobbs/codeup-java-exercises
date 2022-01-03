872d774fb1e6ca98e48b7925892bb7d7c30bab1e
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2022-01-03 12:29:48 -0600

    updated code

--- src/Bob.java
+++ src/Bob.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a question for Bob: ");
+        String userQuestion = scanner.nextLine();
+//        System.out.println(userQuestion.charAt(userQuestion.length() - 1));
 
-    }
-}
+        if (userQuestion.charAt(userQuestion.length() - 1) == '?') {
+            System.out.println("Sure");
+        }else if (userQuestion.charAt(userQuestion.length() - 1) == '!') {
+            System.out.println("Whoa, chill out!");
+        }else if (userQuestion.equals("")) {
+            System.out.println("Fine. Be that way!");
+        }else {
+            System.out.println("Whatever.");
+        }
+
+
+    }// end main
+}// end class Bob
