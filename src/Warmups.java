68352c271a79e8b8740e9c165b6e24f6279bc73c
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2022-01-04 09:07:19 -0600

    Completed morning warmup

--- src/Warmups.java
+++ src/Warmups.java
@@ -1,14 +1,25 @@
 import java.util.Scanner;
 public class Warmups {
+
+    //Create a method called “startDay” that returns a string with the value of “Brain loading… Done!”. Execute your program and print the output to the console.
+    //Add, commit, and push your work to GitHub.
+
+    public static String startDay() {
+        String message = "Brain loading... Done!";
+        return message;
+    }
+
     public static void main(String[] args) {
         //December 15, 2021
-        System.out.println("Java is statically typed, while Javascript is dynamically typed.");
+//        System.out.println("Java is statically typed, while Javascript is dynamically typed.");
+//
+//        //December 16, 2021
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("What did you eat for breakfast today?");
+//        String userInput = scanner.nextLine();
+//
+//        System.out.printf("You ate %s for breakfast today", userInput);
 
-        //December 16, 2021
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("What did you eat for breakfast today?");
-        String userInput = scanner.nextLine();
-
-        System.out.printf("You ate %s for breakfast today", userInput);
+        System.out.println(startDay());
     }// end main
 }//end class
