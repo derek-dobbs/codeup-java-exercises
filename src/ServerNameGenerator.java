dda1aa922a64e20394b1c8c9f42da65cbcd95c84
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2022-01-10 11:51:19 -0600

    Updated exercise

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,18 +1,22 @@
-//public class ServerNameGenerator {
-//
-//
-//    public static void randomName(String adjective, String noun) {
-//        String message = String.format("Your name is: %s-%s", adjective, noun);
-//        System.out.println(message);
-//
-//    }
-//
-//    public static void main(String[] args) {
-//        int random_int = (int)Math.floor(Math.random()*(9-0+1)+0);
-//        String[] adjectives = {"Adorable", "Bad", "Clever", "Dangerous", "Expensive", "Funny", "Grumpy", "Hilarious", "Jolly", "Kind"};
-//
-//        String[] nouns = {"Kangaroo", "Lunch", "Magician", "Notebook", "Oyster", "Planet", "Queen", "Raincoat", "Sandwich", "Train"};
-//    }
-////        randomName(adjectives[random_int], nouns[random_int]);
-//    }// end psvm
-//}//end class ServerNameGenerator
+public class ServerNameGenerator {
+
+    private static String[] adjectives = {"Adorable", "Bad", "Clever", "Dangerous", "Expensive", "Funny", "Grumpy", "Hilarious", "Jolly", "Kind"};
+
+    private static String[] nouns = {"Kangaroo", "Lunch", "Magician", "Notebook", "Oyster", "Planet", "Queen", "Raincoat", "Sandwich", "Train"};
+
+    private static String randomElement (String[] strings) {
+        int random_int = (int)Math.floor(Math.random() * strings.length);
+        return strings[random_int];
+    }
+
+    public static void randomName(String[] adjective, String[] noun) {
+        String adjective1 = randomElement(adjective);
+        String noun1 = randomElement(noun);
+        String message = String.format("Your name is: %s-%s", adjective1, noun1);
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        randomName(adjectives, nouns);
+    }//end psvm
+}//end class ServerNameGenerator
