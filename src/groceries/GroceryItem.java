8125de5e3aa8fa89ff12c019cdaaad5a8360c039
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2022-01-13 09:37:06 -0600

    Updated exercise

---
+++ src/groceries/GroceryItem.java
@@ -0,0 +1,51 @@
+package groceries;
+
+public class GroceryItem {
+    private String item;
+    private String category;
+    private int quantity;
+
+    public GroceryItem(String item) {
+        this.item = item;
+    }
+
+    public GroceryItem(String item, String category) {
+        this.item = item;
+        this.category = category;
+    }
+
+    public GroceryItem(String item, int quantity) {
+        this.item = item;
+        this.quantity = quantity;
+    }
+
+    public GroceryItem(String item, String category, int quantity) {
+        this.item = item;
+        this.category = category;
+        this.quantity = quantity;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
--- src/groceries/GroceryListApplication.java
+++ src/groceries/GroceryListApplication.java
@@ -1,26 +1,55 @@
 package groceries;
+import grades.Student;
 import util.Input;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class GroceryListApplication {
+
+    public static void displayGroceryList (GroceryItem groceryItem1) {
+        String groceryItemName = groceryItem1.getItem();
+        String groceryCategoryName = groceryItem1.getCategory();
+        int groceryQuantity = groceryItem1.getQuantity();
+
+        System.out.printf("Item: %s\tCategory: %s\tQuantity: %s", groceryItemName, groceryCategoryName, groceryQuantity);
+    }
+
     public static void main(String[] args) {
         Scanner intScanner = new Scanner(System.in);
         Scanner stringScanner = new Scanner(System.in);
 
-        System.out.println("Would you like to create a grocery list?");
-        System.out.println("1 - Yes \n2 - No");
+        System.out.println("Would you like to do?");
+        System.out.println("1 - View List\n2 - Delete List\n3 - Add Item to List\n4 - Exit");
         int userListConfirm = intScanner.nextInt();
-        System.out.println("Create list choice: " + userListConfirm);
+//        System.out.println("Create list choice: " + userListConfirm);
         System.out.println();
 
-        System.out.println("Would you like to enter a new item?");
-        System.out.println("1 - Yes \n2 - No");
-        int userNewItemConfirm = intScanner.nextInt();
-        System.out.println("New item confirm choice: " + userNewItemConfirm);
-        System.out.println();
+        switch (userListConfirm) {
+            case 1:
+                System.out.println("Default for #1");
+                break;
+            case 2:
+                System.out.println("Default for #2");
+                break;
+            case 3:
+                //
+                break;
+            case 4:
+                System.out.println("Goodbye");
+                System.exit(0);
+                break;
+        }
 
-        System.out.println("Select the category:" +
+//        System.out.println("Would you like to do?");
+//        System.out.println("1 - Enter new item \n2 - Exit");
+//        int userNewItemConfirm = intScanner.nextInt();
+////        System.out.println("New item confirm choice: " + userNewItemConfirm);
+//        System.out.println();
+
+        System.out.println("Select the item category:" +
                 "\n1 - Grains (bread, cereal, rice, pasta, noodles, etc.)" +
                 "\n2 - Vegetables and Legumes" +
                 "\n3 - Fruit" +
@@ -41,6 +70,36 @@ public class GroceryListApplication {
         System.out.println("User quantity: " + quantity);
         System.out.println();
 
+        String userCategorySelectionString = "default value";
+
+        switch (userCategorySelection) {
+            case 1:
+                userCategorySelectionString = "Grains";
+                break;
+            case 2:
+                userCategorySelectionString = "Vegetables and Legumes";
+                break;
+            case 3:
+                userCategorySelectionString = "Fruit";
+                break;
+            case 4:
+                userCategorySelectionString = "Dairy";
+                break;
+            case 5:
+                userCategorySelectionString = "Meat";
+                break;
+            case 6:
+                userCategorySelectionString = "Other/Uncategorized";
+                break;
+            default:
+                System.out.println("Invalid category entry");
+        }
+
+        HashMap<String, Integer> groceryList = new HashMap<>();
+        groceryList.put(userItem, quantity);
+
+        System.out.println(groceryList);
+
         System.out.println("Please make a selection: " +
                 "\n1 - Finalize List" +
                 "\n2 - Add Item");
@@ -49,7 +108,7 @@ public class GroceryListApplication {
         System.out.println();
 
         System.out.println("Here is your list:");
-        System.out.println("ph_List of Items Organized Alphabetically");
+        System.out.println(groceryList);
         System.out.println();
     }
 }//end class GroceryListApplication
