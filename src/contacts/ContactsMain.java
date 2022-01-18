539df24ff9dd446d7c148a56fde4d5fdc4e5272c
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2022-01-18 10:07:09 -0600

    Added while loop for menu

--- src/contacts/ContactsMain.java
+++ src/contacts/ContactsMain.java
@@ -22,98 +22,105 @@ public class ContactsMain {
 
         boolean invalidEntry = true;
 
-        do {
-            // Main menu
-            System.out.println("Welcome! What would you like to do?" +
-                    "\n////////////////////////////////" +
-                    "\n1. View contacts" +
-                    "\n2. Add a new contact" +
-                    "\n3. Search a contact by name" +
-                    "\n4. Delete an existing contact" +
-                    "\n5. Exit" +
-                    "\n////////////////////////////////");
-            System.out.print("Enter an option: ");
+        boolean startOver = true;
+        while (startOver) {
+            do {
+                // Main menu
+                System.out.println("\nWelcome! What would you like to do?" +
+                        "\n////////////////////////////////" +
+                        "\n1. View contacts" +
+                        "\n2. Add a new contact" +
+                        "\n3. Search a contact by name" +
+                        "\n4. Delete an existing contact" +
+                        "\n5. Exit" +
+                        "\n////////////////////////////////");
+                System.out.print("Enter an option: ");
 
-            int userMenuOption = scanner.nextInt();
+                int userMenuOption = scanner.nextInt();
 //        System.out.println("You entered: " + userMenuOption);
 
-            switch (userMenuOption) {
-                case 1:
+                switch (userMenuOption) {
+                    case 1:
 //                System.out.println("You entered: 1");
-                invalidEntry = false;
-                    // Display all contacts
+                        invalidEntry = false;
+                        // Display all contacts
 
-                    try {
-                        List<String> contacts = Files.readAllLines(dataFile);
-                        for (String contact : contacts) {
-                            System.out.println(contact);
+                        try {
+                            List<String> contacts = Files.readAllLines(dataFile);
+                            Files.write(dataFile, contacts);
+                            for (String contact : contacts) {
+                                System.out.println(contact);
+                            }
+                        }catch (IOException e) {
+                            e.printStackTrace();
                         }
-                    }catch (IOException e) {
-                        e.printStackTrace();
-                    }
 
-                    break;
-                case 2:
+                        break;
+                    case 2:
 //                System.out.println("You entered: 2");
-                    // Add a new contact
-                    System.out.print("Enter the contact's name: ");
-                    String userContactName = scanner.next();
+                        // Add a new contact
+                        System.out.print("Enter the contact's name: ");
+                        String userContactName = scanner.next();
 //                    System.out.println();
 
-                    System.out.print("Enter the contact's phone number: (numbers only, no characters or spaces): ");
-                    String userContactPhoneNumber = scanner.next();
+                        System.out.print("Enter the contact's phone number: (numbers only, no characters or spaces): ");
+                        String userContactPhoneNumber = scanner.next();
 
-                    String nameAndNumber = String.format("%s | %s", userContactName, userContactPhoneNumber);
-                    //Jack Blank | 1231231234
+                        String nameAndNumber = String.format("%s | %s", userContactName, userContactPhoneNumber);
+                        //Jack Blank | 1231231234
 
-                    //TODONE: figure out how to enter input for the contact's name, then move to a new line to enter the contact's phone number. Presently, I am unable to ener the contact name and can only enter the contact number. *Fixed with adding use delimter to scanner and changing scanner.nextline() to scanner.next().
+                        //TODONE: figure out how to enter input for the contact's name, then move to a new line to enter the contact's phone number. Presently, I am unable to ener the contact name and can only enter the contact number. *Fixed with adding use delimter to scanner and changing scanner.nextline() to scanner.next().
 
 //                    System.out.println("Contact name: " + userContactName + "\nContact phone number: " + userContactPhoneNumber);
-                    try {
-                        if (Files.notExists(dataDirectory)) {
-                            Files.createDirectory(dataDirectory);
-                        }
+                        try {
+                            if (Files.notExists(dataDirectory)) {
+                                Files.createDirectory(dataDirectory);
+                            }
 
-                        if (!Files.exists(dataFile)) {
-                            Files.createFile(dataFile);
-                        }
+                            if (!Files.exists(dataFile)) {
+                                Files.createFile(dataFile);
+                            }
 
-                        if (Files.exists(dataFile)) {
-                            // Name | Phone number
-                            //---------------
-                            Files.write(dataFile, Arrays.asList("Name | Phone number"));
-                            Files.write(dataFile, Arrays.asList("---------------"), StandardOpenOption.APPEND);
-                            Files.write(dataFile, Arrays.asList(nameAndNumber), StandardOpenOption.APPEND);
+                            if (Files.exists(dataFile)) {
+                                // Name | Phone number
+                                //---------------
+                                Files.write(dataFile, Arrays.asList("Name | Phone number"));
+                                Files.write(dataFile, Arrays.asList("---------------"), StandardOpenOption.APPEND);
+                                Files.write(dataFile, Arrays.asList(nameAndNumber), StandardOpenOption.APPEND);
 //                            Files.write(dataFile, Arrays.asList(//put variable here that combines name and phone number));
-                            // format: Jack Blank | 1231231234
+                                // format: Jack Blank | 1231231234
+                            }
+                        }catch (IOException e) {
+                            e.printStackTrace();
                         }
-                    }catch (IOException e) {
-                        e.printStackTrace();
-                    }
 
 
-                    invalidEntry = false;
-                    break;
-                case 3:
+                        invalidEntry = false;
+                        break;
+                    case 3:
 //                System.out.println("You entered: 3");
-                    invalidEntry = false;
-                    // Search contact by name
-                    break;
-                case 4:
+                        invalidEntry = false;
+                        // Search contact by name
+                        break;
+                    case 4:
 //                System.out.println("You entered: 4");
-                    invalidEntry = false;
-                    // Delete an existing contact
-                    break;
-                case 5:
-                    System.out.println("Exiting");
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Invalid entry, please try again\n");
-                    break;
-            }//end switch
+                        invalidEntry = false;
+                        // Delete an existing contact
+                        System.out.print("What contact would you like to delete? ");
+                        String userContactToDelete = scanner.next();
+//                    System.out.printf("You deleted: %s", userContactToDelete);
+                        break;
+                    case 5:
+                        System.out.println("Exiting");
+                        System.exit(0);
+                        break;
+                    default:
+                        System.out.println("Invalid entry, please try again\n");
+                        break;
+                }//end switch
 
-        }while (invalidEntry);
+            }while (invalidEntry);
+        }//end while()
 
 //        System.out.println("Exited do while loop");
     }// end psvm
